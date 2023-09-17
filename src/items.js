33a7ee6dacf85dcaec3305a556ba9f1ca99a904e
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2023-09-17 14:27:00 +0300

    Added react item in toolkit

--- src/items.js
+++ src/items.js
@@ -2,6 +2,8 @@ export const ToolkitItems = [
     { name: "C#", img: "public/images/tools/c-sharp-c.228x256.png" },
   
     { name: "JavaScript", img: "public/images/tools/icons8-javascript-96.png" },
+
+    { name: "React", img: "public/images/tools/react.png" }
   
     { name: "HTML5", img: "public/images/tools/icons8-html-5-96.png" },
 
